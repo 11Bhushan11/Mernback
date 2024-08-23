f85777b603780d2253c03284bc72e9365991d293
Sunny Bhushan <sunnybhushan71@gmail.com>
2024-08-24 00:45:50 +0530

    first

--- middlewares/catchAsyncError.js
+++
@@ -1,7 +0,0 @@
-// asynchronus , synchronous error ko handle kare automatically 
-
-export const catchAsyncErrors = (theFunction) => {
-    return (req, res, next) => {
-      Promise.resolve(theFunction(req, res, next)).catch(next);
-    };
-  };
